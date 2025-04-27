f572896b74aca71b1e514a30544d90f683f5b90d
Md Sakib Hasan <sisakib232@gmil.com>
2025-04-27 23:19:56 +0600

    Add project files.

---
+++ Practice Project 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Practice_Project_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Sample sample = new Sample();
+
+            Func<int, int, bool> func = (a, b) => 
+            {
+                if(a>b)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            };
+
+            List<int> list = new List<int>()
+            {
+                10,6,4,8,25,68
+            };
+            sample.bubbleSort(list, func);
+
+            foreach(var val in list)
+            {
+                Console.Write(val+" ");
+            }
+        }
+    }
+}
---
+++ Practice Project 1/Sample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Sample
+{
+    public void bubbleSort(List<int> arr, Func<int,int,bool> func)
+    {
+        int n = arr.Count;
+        bool swapped;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                bool result = func(arr[j], arr[j + 1]);
+                if (result)
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+               
+            }
+
+            if (!swapped)
+                break;
+        }
+    }
+}
